48b30099749c90bb50121b1c0eae0fa40048c73a
Raphaël Colin <raphael.colin68@gmail.com>
2025-10-18 17:31:16 +0200

    examples: create window and OpenGL context for gnu-unifont example

--- examples/gnu-unifont/main.c
+++ examples/gnu-unifont/main.c
@@ -1,6 +1,47 @@
+#include <GL/gl.h>
+#include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  printf("Hello, World!\n");
+void error_callback(int error, const char *description) {
+  fprintf(stderr, "GLFW error: %s\n", description);
 }
 
+int main() {
+  // Set error callback
+  glfwSetErrorCallback(error_callback);
+
+  // Initialize GLFW and handle error
+  if (!glfwInit()) {
+    fprintf(stderr, "failed to initialize GLFW!\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Create window
+  GLFWwindow *window = glfwCreateWindow(
+      640, 480, "rol-font-renderer - GNU Unifont example", NULL, NULL);
+  if (!window) {
+    fprintf(stderr, "failed to create GLFW window!\n");
+    exit(EXIT_FAILURE);
+  }
+  // Make OpenGL context current
+  glfwMakeContextCurrent(window);
+
+  int width;
+  int height;
+  glfwGetFramebufferSize(window, &width, &height);
+  glViewport(0, 0, width, height);
+  glfwSwapInterval(1);
+
+  // Main loop
+  while (!glfwWindowShouldClose(window)) {
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwWaitEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
+}
