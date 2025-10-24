3f9404a141e7f10ec8d6cf881d43b4bc563b5729
Raphaël Colin <raphael.colin68@gmail.com>
2025-10-24 22:30:47 +0200

    examples: update gnu-unifont example to just display a simple 2x2 texture

--- examples/common/shaders/frag.glsl
+++ examples/common/shaders/frag.glsl
@@ -1,8 +1,10 @@
 #version 330 core
 
 out vec4 fragColor;
-in vec3 ourColor;
+in vec2 texCoord;
+
+uniform sampler2D ourTexture;
 
 void main() {
-  fragColor = vec4(ourColor, 1.0f);
+  fragColor = texture(ourTexture, texCoord);
 }
--- examples/gnu-unifont/main.c
+++ examples/gnu-unifont/main.c
@@ -1,11 +1,11 @@
 #include "GL/glew.h"
+#include "shader.h"
 #include <GL/gl.h>
 #include <GL/glext.h>
 #include <GLFW/glfw3.h>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "shader.h"
 
 void error_callback(int error, const char *description) {
   fprintf(stderr, "GLFW error: %s\n", description);
@@ -42,19 +42,40 @@ int main() {
 
   glfwSwapInterval(1);
 
+  // Set byte alignment for texture
+  glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
   // Shader compilation
-  shaderId shaderProgram = compileAndLinkShader("../common/shaders/vertex.glsl", "../common/shaders/frag.glsl");
+  shaderId shaderProgram =
+      compileAndLinkShader("examples/common/shaders/vertex.glsl", "examples/common/shaders/frag.glsl");
   useShader(shaderProgram);
 
-  int ourColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
   // Rectangle data
-  float rectangle[] = {
-    -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-    -0.5f, 0.5f,  0.0f, 0.0f, 1.0f, 0.0f,
-    0.5f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f,
-    0.5f,  -0.5f, 0.0f, 0.0f, 0.0f, 0.0f};
+  // clang-format off
+  float rectangle[] =
+    {
+      -1.0f, -1.0f, 0.0f,
+      0.0f, 0.0f,
+      1.0f, -1.0f, 0.0f,
+      1.0f, 0.0f,
+      -1.0f, 1.0f, 0.0f,
+      0.0f, 1.0f,
+      1.0f, 1.0f, 0.0f,
+      1.0f, 1.0f
+    };
+  // clang-format on
 
-  unsigned int indices[] = {0, 1, 2, 0, 2, 3};
+  unsigned int indices[] = {0, 1, 3, 0, 3, 2};
+
+// clang-format off
+  unsigned char textureData[] =
+  {
+    0, 0, 0,
+    255, 255, 255,
+    255, 255, 255,
+    0, 0, 0
+  };
+// clang-format on
 
   unsigned int VAO;
   glGenVertexArrays(1, &VAO);
@@ -65,16 +86,28 @@ int main() {
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(rectangle), rectangle, GL_STATIC_DRAW);
 
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+
+  unsigned int texture;
+  glGenTextures(1, &texture);
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, texture);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_UNSIGNED_BYTE,
+               textureData);
+
   unsigned int EBO;
   glGenBuffers(1, &EBO);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
-  glEnableVertexAttribArray(0);
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
-                        (void *)(3 * sizeof(float)));
-  glEnableVertexAttribArray(1);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
 
   // Main loop
   while (!glfwWindowShouldClose(window)) {
@@ -86,6 +119,7 @@ int main() {
     // Set shaders and VAO and draw triangle
     useShader(shaderProgram);
     glBindVertexArray(VAO);
+    glBindTexture(GL_TEXTURE_2D, texture);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
     glfwSwapBuffers(window);
     glfwWaitEvents();
